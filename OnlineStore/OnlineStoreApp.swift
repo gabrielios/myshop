17013d7cebdf46bfb9ac7c705dec398dd6047f3f
glebgrib <nekoveclub@gmail.com>
2022-03-24 11:03:27 +0100

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by Gleb Gribov on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
